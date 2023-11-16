b47c233ae6ad1f3274c07916c5f3ddafd1164892
lsw <shaovlee@163.com>
2023-11-16 16:52:07 +0800

    增加py文件

---
+++ lsw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/11/16 16:51
+# @Author  : lsw
+# @Site    : 
+# @File    : lsw.py
+# @Project : GitTest
+# @Software: PyCharm
+import time
